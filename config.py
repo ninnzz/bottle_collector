b986f53fc27d67df7b2834c52937d018d7304ece
cazhiareese <46840412+cazhiareese@users.noreply.github.com>
2019-06-04 20:36:18 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 """
 Configuration class
 """
-
 class Config():
     # TODO
-    APP_NAME = 'Replace with application name'
+    APP_NAME = 'plastic bottle collector'
 
     # Enable debug mode
     DEBUG = True
@@ -15,7 +14,7 @@ class Config():
         'host': 'localhost',
         'db': 'bottle_collector',
         'user': 'root',
-        'password': '',
+        'password': 'jabelojabelo101',
         'port': 3306
     }
 
@@ -31,5 +30,29 @@ class Config():
 
     # TODO
     # This is where frontend should go, create a route for all UI files
+    from flask import Blueprint
+    from flask import render_template
+    
+
+    mod_frontend = Blueprint('frontend', __name__)
+
+
+    @mod_frontend.route('/page1', methods=['GET'])
+    def scan_page():
+        return render_template('pages/page1.html')
+
+    @mod_frontend.route('/page2', methods=['GET'])
+    def profile_page():
+        return render_template('pages/page2.html')
+
+    @mod_frontend.route('/page4', methods=['GET'])
+    def camera_page():
+        return render_template('pages/page4.html')
+
+    @mod_frontend.route('/page5', methods=['GET'])
+    def thankyou_page():
+        return render_template('pages/page5.html')
+
+
     # Setup template folder for webpages
-    TEMPLATE_FOLDER = '/replace/template/folder/here/'
+    TEMPLATE_FOLDER = "C:/Users/cecilia/Documents/programming_practice_l/templates"
